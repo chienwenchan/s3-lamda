f3bc90868ce6b6c48690afc574b6e3770f89e86c
chenjianwen <jianwen.chen@yingxiong.com>
2022-12-14 11:49:42 +0800

    fix

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"log"
+	"time"
 )
 
 type EvEnt struct {
@@ -58,10 +59,16 @@ type Config struct {
 	Split []string `json:"split"`
 }
 
+func GetLocalTimeZone() *time.Location {
+	return time.FixedZone("CST", 8*3600) // UTC+8
+}
+
 func HandleLambdaEvent(event EvEnt) (string, error) {
+	log.Println("start", time.Now().In(GetLocalTimeZone()).Unix())
 	if len(event.Records) < 1 {
 		return "", nil
 	}
+	log.Println("step 1:", time.Now().In(GetLocalTimeZone()).Unix())
 	Bucket := ""
 	jsonFile := ""
 	Record := event.Records[0]
@@ -70,16 +77,20 @@ func HandleLambdaEvent(event EvEnt) (string, error) {
 	if Bucket != "" && jsonFile != "" {
 		return "", nil
 	}
+	log.Println("step 2:", time.Now().In(GetLocalTimeZone()).Unix())
 	svc := s3.New(session.New())
 	input := &s3.GetObjectInput{
 		Bucket: aws.String(Bucket),
 		Key:    aws.String(jsonFile),
 	}
+	log.Println("step 3:", time.Now().In(GetLocalTimeZone()).Unix())
 	result, err := svc.GetObject(input)
 	if err != nil {
 		return "", err
 	}
+	log.Println("step 4:", time.Now().In(GetLocalTimeZone()).Unix())
 	log.Println(result.String())
+	log.Println("step 5:", time.Now().In(GetLocalTimeZone()).Unix())
 	return result.String(), err
 }
 
